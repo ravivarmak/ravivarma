15ef96aa0c19f7f18b2d07ec48fe1027075e991e
RAVIVARMA KANIGANTI <ravivarma.cse@gmail.com>
2024-07-07 16:53:41 +0530

    Update SamlApplication.java

--- saml/src/main/java/ravi/varma/saml/SamlApplication.java
+++ saml/src/main/java/ravi/varma/saml/SamlApplication.java
@@ -2,7 +2,7 @@ package ravi.varma.saml;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Main
 @SpringBootApplication
 public class SamlApplication {
 
