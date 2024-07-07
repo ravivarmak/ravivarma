3808d1816930fddaf52102b4d69a2f401fdcb5f4
RAVIVARMA KANIGANTI <ravivarma.cse@gmail.com>
2024-07-07 16:50:19 +0530

    Update AzureAuthenticationApplication.java

--- azureoauth2/src/main/java/ravi/varma/AzureAuthenticationApplication.java
+++ azureoauth2/src/main/java/ravi/varma/AzureAuthenticationApplication.java
@@ -2,7 +2,7 @@ package ravi.varma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Main
 @SpringBootApplication
 public class AzureAuthenticationApplication {
 
